5485e175ac25b9447c1f0daee1c77fde32016a9a
Pratik Patel <pratikpatelcv@gmail.com>
2020-08-10 10:40:30 +0100

    A product (by unit) has a name identifier, equals and hashCode implemented using name and price

--- src/main/java/kata/supermarket/Product.java
+++ src/main/java/kata/supermarket/Product.java
@@ -1,20 +1,39 @@
 package kata.supermarket;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
-    private final BigDecimal pricePerUnit;
+	private final BigDecimal pricePerUnit;
+	private final String name;
 
-    public Product(final BigDecimal pricePerUnit) {
-        this.pricePerUnit = pricePerUnit;
-    }
+	public Product(final String name, final BigDecimal pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+		this.name = name;
+	}
 
-    BigDecimal pricePerUnit() {
-        return pricePerUnit;
-    }
+	BigDecimal pricePerUnit() {
+		return pricePerUnit;
+	}
 
-    public Item oneOf() {
-        return new ItemByUnit(this);
-    }
+	public Item oneOf() {
+		return new ItemByUnit(this);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, pricePerUnit);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Product))
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name) && Objects.equals(pricePerUnit, other.pricePerUnit);
+	}
+
 }
